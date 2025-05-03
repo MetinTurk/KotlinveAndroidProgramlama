b4a8e969134ca8f25dfc54e9985a1da34224316e
Metin Türk <metinturk553@gmail.com>
2025-05-03 14:26:07 +0300

    öğleden sonra backStage gördük.

--- NavigationComponentKullanimi/app/src/main/java/com/example/navigationcomponentkullanimi/DetayFragment.kt
+++ NavigationComponentKullanimi/app/src/main/java/com/example/navigationcomponentkullanimi/DetayFragment.kt
@@ -1,13 +1,16 @@
 package com.example.navigationcomponentkullanimi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.navArgs
 import com.example.navigationcomponentkullanimi.databinding.FragmentAnasayfaBinding
 import com.example.navigationcomponentkullanimi.databinding.FragmentDetayBinding
+import kotlin.math.log
 
 
 class DetayFragment : Fragment() {
@@ -23,6 +26,14 @@ class DetayFragment : Fragment() {
         val gelenSayi = bundle.sayi
 
         binding.textViewSonuc.text = "$gelenMesaj - $gelenSayi"
+
+        val geriTusu = object : OnBackPressedCallback(false){//true: geri dönüş pasif
+            override fun handleOnBackPressed() {
+                Log.e("Detay sayfa" , "Geri tuşu çalıştı")
+                isEnabled = false   // eğer yinede geri döndürmek istiyorsak bunu yapıyoruz.
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,geriTusu)   //detay frag. üzerinde çalışan activity temsil ediyo (main activity)
         return binding.root
     }
 }
