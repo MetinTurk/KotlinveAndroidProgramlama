ed08f8b1719fba813c7b91061a45bb946a74bada
Metin Türk <metinturk553@gmail.com>
2025-05-10 16:45:27 +0300

    son ders de bitti DataStore kullanımına başladık devam edeceğiz.

---
+++ DatastoreKullanimi/app/src/main/java/com/example/datastorekullanimi/AppPref.kt
@@ -0,0 +1,12 @@
+package com.example.datastorekullanimi
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+class AppPref(var context: Context) {
+    val Context.ds : DataStore<Preferences> by preferencesDataStore("bilgiler") //bilgiler = Dosya adı
+
+    
+}
