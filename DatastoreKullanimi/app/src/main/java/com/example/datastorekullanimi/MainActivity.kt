749021e1d6b19a1e4c4b6d476998608b50081d16
Metin Türk <metinturk553@gmail.com>
2025-05-13 19:37:52 +0300

    DataStore kullanımı bitti.

--- DatastoreKullanimi/app/src/main/java/com/example/datastorekullanimi/MainActivity.kt
+++ DatastoreKullanimi/app/src/main/java/com/example/datastorekullanimi/MainActivity.kt
@@ -6,15 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.datastorekullanimi.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding :  ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-
         setContentView(binding.root)
+
+        val appPref = AppPref(this)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            //appPref.sil() // hepsini sıfırlar
+            var gelenSayac = appPref.oku()
+
+
+            appPref.kayit(++gelenSayac)
+
+            binding.textviewSayac.text = "Açılış Sayısı: ${gelenSayac}"
+        }
 
     }
 }
